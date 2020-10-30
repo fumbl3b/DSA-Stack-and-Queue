9d2043ad82b68e15212baeb2e26032e09940a2d9
Harrison Winkler <harryewinkler@thinkful.com>
2020-10-30 12:51:49 -0400

    check parens function works

--- tests.js
+++ tests.js
@@ -21,4 +21,46 @@ function main() {
 
 }
 
-main();
+function is_palindrome(s) {
+  s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+
+  const stack = new Stack();
+  for(let i = s.length - 1; i >= 0; i--) {
+    stack.push(s[i]);
+    if(stack.top.data !== s[s.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function checkParens(s) {
+  const stack = new Stack();
+
+  for(let i = 0; i < s.length; i++) {
+    if(s[i] === '(') {
+      stack.push(i);
+    }
+    if(s[i] === ')') {
+      if(stack.isEmpty()) {
+        console.log(`extra closing parens at index ${i}`);
+        return false;
+      } else{
+        stack.pop();
+      }
+    }
+  }
+  if(!stack.isEmpty()) {
+    console.log(`missing closing parens for '(' at index ${stack.top.data}`);
+    return false;
+  }
+  return true;
+}
+
+// True, true, true, false
+console.log('asdf + ((4x) + 9) + (3/(8+2))', checkParens('asdf + ((4x) + 9) + (3/(8+2))'));
+console.log('(()()()))())(', checkParens('(()()()))())('));
+console.log('((())))', checkParens('((())))'));
+console.log('(hello)(hello)(hello', checkParens('(hello)(hello)(hello'));
+
+//main();
